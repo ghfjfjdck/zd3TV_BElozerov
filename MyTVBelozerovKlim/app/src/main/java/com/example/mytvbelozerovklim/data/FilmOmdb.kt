336590d988271715af8bd4f3dae2bcfd414938d2
ghfjfjdck <157564400+ghfjfjdck@users.noreply.github.com>
2024-12-23 18:10:23 +0500

    Delete MyTVBelozerovKlim directory

--- MyTVBelozerovKlim/app/src/main/java/com/example/mytvbelozerovklim/data/FilmOmdb.kt
+++
@@ -1,9 +0,0 @@
-package com.example.mytvbelozerovklim.data
-
-data class FilmOmdb(
-    val Title: String,
-    val Year: String,
-    val imdbID: String,
-    val Type: String,
-    val Poster: String
-)
--- MyTVBelozerovKlim/app/src/main/java/com/example/mytvbelozerovklim/data/FilmResponse.kt
+++
@@ -1,8 +0,0 @@
-package com.example.mytvbelozerovklim.data
-
-data class FilmResponse(
-    val Search: List<FilmOmdb>?,
-    val totalResults: String?,
-    val Response: String,
-    val Error: String?
-)
